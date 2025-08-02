7d1d7730c408c75d5fd34fb8ebd8abc40c0f29fd
Robert Mads Christensen <61755754+madschristensen99@users.noreply.github.com>
2025-08-02 19:31:14 -0400

    Update DeployContracts.s.sol

--- script/DeployContracts.s.sol
+++ script/DeployContracts.s.sol
@@ -3,7 +3,8 @@ pragma solidity ^0.8.19;
 
 import {Script, console2} from "forge-std/Script.sol";
 import {SwapCreator} from "../contracts/atomic-swap/ethereum/contracts/SwapCreator.sol";
-import {SwapCreatorAdapter} from "../contracts/SwapCreatorAdapter.sol";
+import {XMREscrowSrc} from "../contracts/XMREscrowSrc.sol";
+import {XMREscrowDst} from "../contracts/XMREscrowDst.sol";
 
 contract DeployContracts is Script {
     function run() external {
@@ -15,9 +16,13 @@ contract DeployContracts is Script {
         SwapCreator swapCreator = new SwapCreator();
         console2.log("SwapCreator deployed at:", address(swapCreator));
         
-        // Deploy SwapCreatorAdapter with the SwapCreator address
-        SwapCreatorAdapter swapCreatorAdapter = new SwapCreatorAdapter(swapCreator);
-        console2.log("SwapCreatorAdapter deployed at:", address(swapCreatorAdapter));
+        // Deploy XMREscrowSrc with the SwapCreator address
+        XMREscrowSrc xmrEscrowSrc = new XMREscrowSrc(swapCreator);
+        console2.log("XMREscrowSrc deployed at:", address(xmrEscrowSrc));
+        
+        // Deploy XMREscrowDst with the SwapCreator address
+        XMREscrowDst xmrEscrowDst = new XMREscrowDst(swapCreator);
+        console2.log("XMREscrowDst deployed at:", address(xmrEscrowDst));
         
         vm.stopBroadcast();
     }
